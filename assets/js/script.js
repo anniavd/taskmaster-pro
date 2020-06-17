599d6a65f74d05eeb4fd6a7545677db87631a6db
annia valdes <millerl.mb79@gmail.com>
2020-06-16 22:13:34 -0500

    add a fucntion sortable

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,6 +140,29 @@ $(".list-group").on("blur", "input[type='text']", function() {
 });
 
 
+ //  fuction Sortable (drop zone a dn drag)
+ $(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    console.log($(this).children());
+  }
+});
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
